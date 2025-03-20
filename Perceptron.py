0cc009cab4feb4b2f7639d9938a5f010f47642ea
andrewmalley725 <andrew.alley99@gmail.com>
2025-03-20 16:22:46 -0500

    added annotations to perceptron class

--- Perceptron.py
+++ Perceptron.py
@@ -6,30 +6,30 @@ class Perceptron:
     def fit(self, X, y):
         import numpy as np
         n,d = X.shape
-        self.weights = np.zeros(d)
+        self.weights = np.zeros(d) #init weights as a d dimensional 0 vector
         self.bias = 0.0
-        w = np.append(self.weights, self.bias)
-        X = np.hstack((X, np.ones((n, 1))))
+        w = np.append(self.weights, self.bias) #append bias to weights
+        X = np.hstack((X, np.ones((n, 1)))) #append 1 to each row of X
         iter = 0
         while True:
             iter += 1
-            m = 0
+            m = 0 #number of misclassified points
             for i in range(n):
-                if y[i] * np.dot(w,X[i]) <= 0:
-                    w += y[i] * X[i]
+                if y[i] * np.dot(w,X[i]) <= 0: #if signs of true label and predicted label are different
+                    w += y[i] * X[i] #move hyperplane to correctly classify point
                     m += 1
-            if m == 0:
+            if m == 0: #if no misclassified points, break
                 break
         self.weights = w[:-1]
         self.bias = w[-1]
-        return self.weights, self.bias
+        return self.weights, self.bias, iter
     
     def predict(self, X):
         import numpy as np
         n,d = X.shape
         w = np.append(self.weights, self.bias)
         X = np.hstack((X, np.ones((n, 1))))
-        y_pred = np.sign(np.dot(X, w))
+        y_pred = np.sign(np.dot(X, w)) #sign determine side of hyperplane
         return y_pred
 
 
